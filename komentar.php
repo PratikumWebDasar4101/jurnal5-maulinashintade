63711b3c28ff6e902e1868b7e2581920d57ff638
maulinashintade <42725367+maulinashintade@users.noreply.github.com>
2018-10-01 10:56:50 +0700

    Update komentar.php

--- komentar.php
+++ komentar.php
@@ -1,7 +1,19 @@
-<?php
-//untuk yg komentar kalian bebas mau menggunakan 2 file atau satu file php
+<form method="post">
+ 	<center><h2>Masukkan Komentar Anda</h2></center>
+ 	<center><b>Komentar : </b><textarea name="komentar" rows="5" cols="40"></textarea></center><br>
+ 	<center><input type="submit" name="submit" value="Kirim"></center>
+ </form>
 
+ <?php 
+if (isset($_POST['submit'])) {
+	$komentar = $_POST['komentar'];
 
+	$count = explode(" ", $komentar);
+	if (count($count) < 5) {
+		echo "Jumlah kata di komentar tidak sampai 5 kata";
+	}else{
+		echo "Jumlah kata di komentar : ".count($count)."kata";
+	}
+}
 
-
-?>
+ ?>
