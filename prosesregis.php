1201cf7b3fb442b84184fe862db0a6ecacd95965
maulinashintade <42725367+maulinashintade@users.noreply.github.com>
2018-10-01 10:56:20 +0700

    Update prosesregis.php

--- prosesregis.php
+++ prosesregis.php
@@ -1,7 +1,16 @@
 <?php
-
-
-
-
-
-?>
+session_start();
+$nim = $_POST['nim'];
+$nama = $_POST['nama'];
+$email = $_POST['email'];
+if (isset($_POST['submit'])) {
+	$nim   = isset($_POST['nim']) ? $_POST['nim'] : "";
+	$nama  = isset($_POST['nama']) ? $_POST['nama'] : "";
+	$email = isset($_POST['email']) ? $_POST['email'] : "";	
+}
+echo "Nim   : " .$nim;
+echo "<br>";
+echo "Nama  : " .$nama;
+echo "<br>";
+echo "Email : " .$email;
+ ?>
